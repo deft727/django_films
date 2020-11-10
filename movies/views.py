70f8619cd328b393b78916579388002a75114d61
deft727 <deft727@gmail.com>
2020-11-10 13:29:29 +0200

    movie filter

--- movies/views.py
+++ movies/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render,redirect
 from django.views.generic.base import View
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
-from .models import Movie,Category,Actor
+from .models import Movie,Category,Actor,Ganre
 from .forms import RewiewForm
+from django.db.models import Q
 
-class MoviesView(ListView):
+
+class GanreYear():
+    def get_genres(self):
+        return Ganre.objects.all()
+    
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GanreYear,ListView):
     model=Movie
     queryset=Movie.objects.filter(draft=False)
     # template_name="movies/movies.html"
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GanreYear,DetailView):
     model=Movie
     slug_field="url"
 
@@ -31,7 +41,15 @@ class AddReview(View):
         return redirect(movie.absoluteUrl())
 
 
-class ActorView(DetailView):
+class ActorView(GanreYear,DetailView):
     model=Actor
     template_name='movies/actor.html'
     slug_field="name"
+
+class FilterMoviesView(GanreYear,ListView):
+    def get_queryset(self):
+        queryset=Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+            )
+        return queryset
